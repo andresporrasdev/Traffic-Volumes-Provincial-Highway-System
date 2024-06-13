c3dfa24eb8a107202d00b7fe894d6913d771d64e
Andres Porras <camilop1730@gmail.com>
2024-06-13 17:23:20 -0400

    Adding new variables

--- trafficdata/traffic_data_app/views.py
+++ trafficdata/traffic_data_app/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from .models import TrafficData
 from .utils.ReadCSV import readCSV  # Add this line
 
+traffic_data = []
+
 def index(request):  # Add this function
     # return render(request, 'traffic_data.html')
     traffic_data = readCSV()
-    return render(request, 'traffic_data.html', {'traffic_data': readCSV()})
+    return render(request, 'traffic_data.html', {'traffic_data': traffic_data})
 
 def traffic_data_view(request):
     # fileName = "./traffic_data_app/data/Traffic_Volumes_-_Provincial_Highway_System.csv"
