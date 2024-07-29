3f22a4a5fcfa13995128f7c46a1f37e61a837584
Andres Porras <camilop1730@gmail.com>
2024-07-29 15:56:24 -0400

    Added Charts visualization

--- trafficdata/trafficdata/urls.py
+++ trafficdata/trafficdata/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     path('delete_selected/', views.delete_selected_data, name='delete_selected_data'),
     path('insert/', views.insert_data, name='insert_data'),
     path('display_selected/', views.display_selected_data, name='display_selected_data'),
+    path('charts/', views.traffic_data_chart, name='chart_data'),
 ]
